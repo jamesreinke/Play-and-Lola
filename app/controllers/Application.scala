2fc174d0841a605eacb287e3f899d943a48352ee
jamesreinke <james@rtsystems.io>
2015-10-25 08:03:11 -0700

    ~

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,15 +13,13 @@ class Application extends Controller {
   }
 
   def home = Action {
-  	
-    val a = for(a <- 1 until 20) yield Article("James Wins the Election", 
-            "10-12-2020", 
-            "James Reinke", 
-            "It was an amazing election where James just barely won the vote at 79%..." * 20)
-  	Ok(
-  		Encode(
-          (for(article <- a) yield new Create(article)).toList ++ 
-          (for(article <- a) yield new OnClick(article, new SlideUp(article, 1000))).toList))
+  	val header = List("Firstname", "Lastname", "Email")
+    val table = List(
+      List("John", "Doe", "john@example.com"),
+      List("Mary", "Moe", "mary@example.com"),
+      List("July", "Dooley", "july@example.com")
+      )
+    Ok(Encode(new Create(DatabaseView(header = header, table = table))))
   }
 
 }
--- app/modules/Modules.scala
+++ app/modules/Modules.scala
@@ -3,7 +3,7 @@ package modules
 import lola.interface._
 
 /*
-	An example how primitive node values can be used...
+	Example: Primitive node
 */
 object Article {
 
@@ -13,10 +13,25 @@ object Article {
 	}
 
 	def apply(title: String, date: String, author: String, content: String): Node = {
-		val t = el("h1", attributes = col(12), style = Map("text-align" -> "center"), sText = title)
-		val d = el("div", attributes = col(2), style = Map("float" -> "right"), sText = date)
-		val a = el("div", style = Map("float"->"right"), attributes = col(2), sText = author)
-		val c = el("div", attributes = col(12), sText = content, style = Map("padding" -> "50px"))
+		val t = el("h1", attributes = col(12), style = Map("text-align" -> "center"), text = title)
+		val d = el("div", attributes = col(2), style = Map("float" -> "right"), text = date)
+		val a = el("div", style = Map("float"->"right"), attributes = col(2), text = author)
+		val c = el("div", attributes = col(12), text = content, style = Map("padding" -> "50px"))
 		el("div", attributes = col(12), items = List(t, d, a, c))
+	}
+}
+
+/*
+	Turns an object into a database view.  Plans to add features such as search filter, page toggling...
+*/
+object DatabaseView {
+
+	def apply(header: List[String] = List(), table: List[List[String]]): Node = {
+		val labels = el("tr", items = header map { x => el("th", text = x) })
+		el(
+			"table", 
+			attributes = Map("class" -> "table"),
+			items = labels +: (table map { row => el("tr", items = row map { col => el("td", text = col) }) })
+		)
 	}
 }
