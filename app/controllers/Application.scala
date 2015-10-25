c66bd7974c794588aaec093e0b99620482aee60a
jamesreinke <james@rtsystems.io>
2015-10-25 13:46:59 -0700

    ~

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,29 +13,6 @@ class Application extends Controller {
   }
 
   def home = Action {
-  	val header = List("Firstname", "Lastname", "Email")
-    val table = List(
-      List("John", "Doe", "john@example.com"),
-      List("Mary", "Moe", "mary@example.com"),
-      List("July", "Dooley", "july@example.com"),
-      List("Belle", "Tuttle", "btuts@silkemail.com"),
-      List("James", "Reinke", "JaMez@silkemail.com")
-      )
-    val database = DatabaseView(header = header, table = table)
-    val input = el("input", style=Map("margin" -> "25px"))
-    Ok(
-      Encode(
-        List(
-          new Create(input),
-          new OnKeyUp(input, new Post("/database/filter", database)),
-          new Create(database))))
+    Ok("")
   }
-
-  def databaseFilter = Action { 
-    implicit request => {
-      val db = Decode(request.body.asText.getOrElse(""))
-      Ok(Encode(DatabaseView.filter(db, "John")))
-    }
-  }
-
 }
