fddf14f575795701c739db863795aaf4c1327f03
jamesreinke <james@rtsystems.io>
2015-10-25 15:01:28 -0700

    ~

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,18 +13,22 @@ class Application extends Controller {
 
   def home = Action {
     val node = el("p", text = "whatever")
-    val cms = List(new Create(node), new OnClick(node, new Post("/delete", node)))
+    val input = el("input", attributes = Map("class" -> "input"))
+
+    val cms = 
+      List(new Create(node), new OnClick(node, new Post("/change", List(node)))) ++
+      List(new Create(input), new OnKeyUp(input, new Post("/change", List(node))))
 
     Ok(Encode(cms))
   }
 
-  def delete = Action {
+  def change = Action {
     implicit request => {
       request.body.asText match {
         case Some(text) => {
-          val p = Decode(text)
+          val p = Decode(text).head
           p.style = Map("text-align" -> "center")
-          p.text = "Do not deny me!"
+          p.text = "Do not deny me!  " + p.text
            Ok(Encode(new Update(p)))
          }
         case None => BadRequest("Did not receive a node.")
