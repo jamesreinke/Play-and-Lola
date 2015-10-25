b9967dcc0d8c299cc5fdc96cc489ce51377c8a72
jamesreinke <james@rtsystems.io>
2015-10-25 13:57:51 -0700

    ~

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,6 @@ import play.api._
 import play.api.mvc._
 import upickle.default._
 import lola.interface._
-import modules._
 
 class Application extends Controller {
 
@@ -13,6 +12,23 @@ class Application extends Controller {
   }
 
   def home = Action {
-    Ok("")
+    val node = el("p", text = "whatever")
+    val cms = List(new Create(node), new OnClick(node, new Post("/delete", node)))
+
+    Ok(Encode(cms))
+  }
+
+  def delete = Action {
+    implicit request => {
+      request.body.asText match {
+        case Some(text) => {
+          val p = Decode(text)
+          p.style = Map("text-align" -> "center")
+          p.text = "Do not deny me!"
+           Ok(Encode(new Update(p)))
+         }
+        case None => BadRequest("Did not receive a node.")
+      }
+    }
   }
 }
