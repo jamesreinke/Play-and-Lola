4268d68c07e9bda11d4e6305f33c4422cd0951cb
jamesreinke <james@rtsystems.io>
2015-10-24 19:32:06 -0700

    ~

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,15 +14,14 @@ class Application extends Controller {
 
   def home = Action {
   	
-    val a = Article("James Wins the Election", 
+    val a = for(a <- 1 until 20) yield Article("James Wins the Election", 
             "10-12-2020", 
             "James Reinke", 
             "It was an amazing election where James just barely won the vote at 79%..." * 20)
   	Ok(
   		Encode(
-	  		List(
-          new Create(a),
-          new OnClick(a, new FadeOut(a, 2000)))))
+          (for(article <- a) yield new Create(article)).toList ++ 
+          (for(article <- a) yield new OnClick(article, new SlideUp(article, 1000))).toList))
   }
 
 }
